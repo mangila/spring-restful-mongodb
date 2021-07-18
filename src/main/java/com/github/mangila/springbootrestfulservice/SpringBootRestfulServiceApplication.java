9dda21db1d74faf70d70cbc81945e1c67d856d99
erik <olsson.erik1993@gmail.com>
2021-07-18 16:57:56 +0200

    bootstrap spring boot project

---
+++ src/main/java/com/github/mangila/springbootrestfulservice/SpringBootRestfulServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.mangila.springbootrestfulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestfulServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/mangila/springbootrestfulservice/SpringBootRestfulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mangila.springbootrestfulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfulServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
