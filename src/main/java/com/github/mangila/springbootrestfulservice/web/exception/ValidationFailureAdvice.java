0cd424f11d46a3978288970b8ac4ddf572040f8e
erik <olsson.erik1993@gmail.com>
2021-07-23 12:51:24 +0200

    added validation controller advice

---
+++ src/main/java/com/github/mangila/springbootrestfulservice/web/exception/ValidationFailureAdvice.java
@@ -0,0 +1,26 @@
+package com.github.mangila.springbootrestfulservice.web.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class ValidationFailureAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> buildResponseValidationFailed(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+}
