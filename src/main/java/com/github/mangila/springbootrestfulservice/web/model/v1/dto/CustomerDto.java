80b99ca571f3db092cc832525fdc602c21a82a95
erik <olsson.erik1993@gmail.com>
2021-07-23 11:58:42 +0200

    added Bean Validation

--- src/main/java/com/github/mangila/springbootrestfulservice/web/model/v1/dto/CustomerDto.java
+++ src/main/java/com/github/mangila/springbootrestfulservice/web/model/v1/dto/CustomerDto.java
@@ -3,9 +3,21 @@ package com.github.mangila.springbootrestfulservice.web.model.v1.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.PastOrPresent;
+import java.time.LocalDate;
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 public class CustomerDto {
 
+    @Null
     private String id;
+    @NotBlank
+    private String name;
+    @PastOrPresent
+    private LocalDate registration;
+    private List<String> orderHistory;
 }
--- src/main/java/com/github/mangila/springbootrestfulservice/web/model/v1/dto/OrderDto.java
+++ src/main/java/com/github/mangila/springbootrestfulservice/web/model/v1/dto/OrderDto.java
@@ -1,11 +1,25 @@
 package com.github.mangila.springbootrestfulservice.web.model.v1.dto;
 
+import com.github.mangila.springbootrestfulservice.domain.Address;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 public class OrderDto {
 
+    @Null
     private String id;
+    @NotNull
+    private List<String> products;
+    @NotNull
+    @Positive
+    private Integer amount;
+    @NotNull
+    private Address address;
 }
