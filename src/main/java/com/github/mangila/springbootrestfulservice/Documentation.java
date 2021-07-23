68b63e57eadc7498d432e161c7d3fa038241c8e8
erik <olsson.erik1993@gmail.com>
2021-07-23 19:02:32 +0200

    added OpenAPI documentation

---
+++ src/main/java/com/github/mangila/springbootrestfulservice/Documentation.java
@@ -0,0 +1,31 @@
+package com.github.mangila.springbootrestfulservice;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Documentation {
+
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${application-description}") String appDescription,
+                                 @Value("${application-version}") String appVersion,
+                                 @Value("${application-name}") String appName) {
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title(appName)
+                        .version(appVersion)
+                        .description(appDescription)
+                        .termsOfService("http://swagger.io/terms/")
+                        .contact(new Contact()
+                                .name("Mangila"))
+                        .license(new License().name("MIT").url("https://github.com/mangila/spring-boot-restful-service/blob/master/LICENSE")));
+    }
+
+}
