fc82629870f790486fffd05816b6a33569ff3336
erik <olsson.erik1993@gmail.com>
2021-07-23 15:59:03 +0200

    adding CRUD operations

--- src/main/java/com/github/mangila/springbootrestfulservice/web/resource/v1/CustomerResource.java
+++ src/main/java/com/github/mangila/springbootrestfulservice/web/resource/v1/CustomerResource.java
@@ -1,15 +1,21 @@
 package com.github.mangila.springbootrestfulservice.web.resource.v1;
 
 import com.github.mangila.springbootrestfulservice.domain.CustomerDocument;
+import com.github.mangila.springbootrestfulservice.web.model.v1.dto.CustomerDto;
 import com.github.mangila.springbootrestfulservice.web.service.v1.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import java.util.List;
 
+@Validated
 @RequestMapping("v1/customer")
 @RestController
 public class CustomerResource {
@@ -28,21 +34,45 @@ public class CustomerResource {
 
     @GetMapping("{id}")
     public ResponseEntity<CustomerDocument> findById(@PathVariable String id) {
-        var c = this.service.findById(id);
-        if (c.isPresent()) {
-            return ResponseEntity.ok(c.get());
+        if (this.service.existsById(id)) {
+            return ResponseEntity.ok(this.service.findById(id));
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("'%s' not found", id));
         }
     }
 
-    @GetMapping("find")
-    public ResponseEntity<CustomerDocument> findByName(@RequestParam(name = "name") String name) {
-        var c = this.service.findByName(name);
-        if (c != null) {
-            return ResponseEntity.ok(c);
+    @PostMapping
+    public ResponseEntity<?> insertNewCustomer(@Valid @RequestBody CustomerDto customerDto,
+                                               HttpServletRequest request) {
+        var id = this.service.insertNewCustomer(customerDto);
+        var headers = new HttpHeaders();
+        headers.add(HttpHeaders.LOCATION, request.getRequestURL().append("/").append(id).toString());
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<?> updateCustomer(@PathVariable String id,
+                                            @Valid @RequestBody CustomerDto customerDto,
+                                            HttpServletRequest request) {
+        var headers = new HttpHeaders();
+        if (this.service.existsById(id)) {
+            this.service.updateCustomer(id, customerDto);
+            headers.add(HttpHeaders.LOCATION, request.getRequestURL().append("/").append(id).toString());
+            return new ResponseEntity<>(headers, HttpStatus.NO_CONTENT);
         } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("'%s' not found", name));
+            var newId = this.service.insertNewCustomer(customerDto);
+            headers.add(HttpHeaders.LOCATION, request.getRequestURL().append("/").append(newId).toString());
+            return new ResponseEntity<>(headers, HttpStatus.CREATED);
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> deleteById(@PathVariable String id) {
+        if (this.service.existsById(id)) {
+            this.service.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("'%s' not found", id));
         }
     }
 
