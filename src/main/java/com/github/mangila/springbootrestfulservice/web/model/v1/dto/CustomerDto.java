1f8c5cfad0cbb410632ce75d65a8a9ede10cffc3
erik <olsson.erik1993@gmail.com>
2021-07-23 12:14:07 +0200

    added Bean Validation

--- src/main/java/com/github/mangila/springbootrestfulservice/web/model/v1/dto/CustomerDto.java
+++ src/main/java/com/github/mangila/springbootrestfulservice/web/model/v1/dto/CustomerDto.java
@@ -3,9 +3,7 @@ package com.github.mangila.springbootrestfulservice.web.model.v1.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Null;
-import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -16,6 +14,7 @@ public class CustomerDto {
     @Null
     private String id;
     @NotBlank
+    @Size(min = 3, max = 100)
     private String name;
     @PastOrPresent
     private LocalDate registration;
