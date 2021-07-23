d19ce522d5dee7c06cee4caf3d65b06af0d58e7f
erik <olsson.erik1993@gmail.com>
2021-07-23 17:02:05 +0200

    added validation

--- src/main/java/com/github/mangila/springbootrestfulservice/domain/Address.java
+++ src/main/java/com/github/mangila/springbootrestfulservice/domain/Address.java
@@ -4,10 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Address {
+    @NotNull
     private String street;
+    @NotNull
     private String city;
 }
