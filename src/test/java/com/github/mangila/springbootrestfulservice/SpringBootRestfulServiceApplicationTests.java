277a0d98c74abbb8f068ffaf639011ea0a1378b4
erik <olsson.erik1993@gmail.com>
2021-07-26 14:59:35 +0200

    testing circleci pipeline

--- src/test/java/com/github/mangila/springbootrestfulservice/SpringBootRestfulServiceApplicationTests.java
+++ src/test/java/com/github/mangila/springbootrestfulservice/SpringBootRestfulServiceApplicationTests.java
@@ -1,13 +1,15 @@
 package com.github.mangila.springbootrestfulservice;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootRestfulServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        Assertions.assertEquals(1 + 1, 3);
+    }
 
 }
