aaed1558bb60a20996dee5079d8bdbf90b3d74f5
erik <olsson.erik1993@gmail.com>
2021-07-26 16:08:40 +0200

    added Mockito MockMVC test n removed SpringBoot test

---
+++ src/test/java/com/github/mangila/springbootrestfulservice/web/resource/v1/OrderResourceTest.java
@@ -0,0 +1,38 @@
+package com.github.mangila.springbootrestfulservice.web.resource.v1;
+
+import com.github.mangila.springbootrestfulservice.web.service.v1.OrderService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(OrderResource.class)
+class OrderResourceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private OrderService service;
+
+    @Test
+    void findAll() {
+        //TODO impl
+    }
+
+    @Test
+    void findById() {
+        //TODO impl
+    }
+
+    @Test
+    void insertNewOrder() {
+        //TODO impl
+    }
+
+    @Test
+    void deleteById() {
+        //TODO impl
+    }
+}
