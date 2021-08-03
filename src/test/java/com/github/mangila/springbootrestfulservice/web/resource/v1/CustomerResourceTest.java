df301cfe468ab69be596664d3f2d4c679a035019
Mangila <olsson.erik1993@gmail.com>
2021-08-03 19:48:52 +0200

    decorated with tag annotation for testing

--- src/test/java/com/github/mangila/springbootrestfulservice/web/resource/v1/CustomerResourceTest.java
+++ src/test/java/com/github/mangila/springbootrestfulservice/web/resource/v1/CustomerResourceTest.java
@@ -1,6 +1,7 @@
 package com.github.mangila.springbootrestfulservice.web.resource.v1;
 
 import com.github.mangila.springbootrestfulservice.web.service.v1.CustomerService;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -10,6 +11,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 
+@Tag("resources")
 @WebMvcTest(CustomerResource.class)
 class CustomerResourceTest {
 
--- src/test/java/com/github/mangila/springbootrestfulservice/web/resource/v1/OrderResourceTest.java
+++ src/test/java/com/github/mangila/springbootrestfulservice/web/resource/v1/OrderResourceTest.java
@@ -1,12 +1,14 @@
 package com.github.mangila.springbootrestfulservice.web.resource.v1;
 
 import com.github.mangila.springbootrestfulservice.web.service.v1.OrderService;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+@Tag("resources")
 @WebMvcTest(OrderResource.class)
 class OrderResourceTest {
 
