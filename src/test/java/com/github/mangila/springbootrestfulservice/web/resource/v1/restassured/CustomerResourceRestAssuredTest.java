6e03e70e6546c2688e35a246dc65b8893ed89738
Mangila <olsson.erik1993@gmail.com>
2021-08-28 23:29:04 +0200

    added REST-assured dependency

---
+++ src/test/java/com/github/mangila/springbootrestfulservice/web/resource/v1/restassured/CustomerResourceRestAssuredTest.java
@@ -0,0 +1,7 @@
+package com.github.mangila.springbootrestfulservice.web.resource.v1.restassured;
+
+import org.junit.jupiter.api.Tag;
+
+@Tag("restassured")
+public class CustomerResourceRestAssuredTest {
+}
