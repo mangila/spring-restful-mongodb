b0a00972ed13c265fc3aa25275661165589ba135
Mangila <olsson.erik1993@gmail.com>
2021-08-30 01:00:44 +0200

    updated pom.xml

--- src/test/java/com/github/mangila/springbootrestfulservice/web/resource/v1/restassured/CustomerResourceRestAssuredTest.java
+++ src/test/java/com/github/mangila/springbootrestfulservice/web/resource/v1/restassured/CustomerResourceRestAssuredTest.java
@@ -1,7 +1,41 @@
 package com.github.mangila.springbootrestfulservice.web.resource.v1.restassured;
 
+import com.github.mangila.springbootrestfulservice.domain.OrderDocument;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
 
 @Tag("restassured")
 public class CustomerResourceRestAssuredTest {
+
+    @AfterEach
+    void afterEach() {
+
+    }
+
+    @Test
+    void findAll() {
+
+    }
+
+    @Test
+    void findById() {
+
+    }
+
+    @Test
+    void update() {
+
+    }
+
+    @Test
+    void deleteById() {
+
+    }
+
 }
