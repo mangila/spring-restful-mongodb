d3627aabdc021b43ad4bf569468631446d0434d3
Mangila <olsson.erik1993@gmail.com>
2021-09-01 17:43:10 +0200

    added RestAssuredMockMVC dependency

--- src/test/java/com/github/mangila/springbootrestfulservice/web/resource/v1/restassured/CustomerResourceRestAssuredTest.java
+++ src/test/java/com/github/mangila/springbootrestfulservice/web/resource/v1/restassured/CustomerResourceRestAssuredTest.java
@@ -1,26 +1,45 @@
 package com.github.mangila.springbootrestfulservice.web.resource.v1.restassured;
 
-import com.github.mangila.springbootrestfulservice.domain.OrderDocument;
-import org.assertj.core.util.Lists;
-import org.junit.jupiter.api.AfterEach;
+import com.github.mangila.springbootrestfulservice.web.dto.v1.CustomerDto;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 
-import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
-import static org.hamcrest.Matchers.*;
+import java.util.Arrays;
+
+import static io.restassured.RestAssured.given;
 
 @Tag("restassured")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CustomerResourceRestAssuredTest {
 
-    @AfterEach
-    void afterEach() {
+    @LocalServerPort
+    private int port;
 
+    @BeforeEach
+    void afterEach() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = this.port;
+        RestAssured.basePath = "/api/v1";
     }
 
     @Test
     void findAll() {
-
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .get("customer")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .extract().response();
+        CustomerDto[] body = response.getBody().as(CustomerDto[].class);
     }
 
     @Test
--- src/test/java/com/github/mangila/springbootrestfulservice/web/resource/v1/restassured/OrderResourceRestAssuredTest.java
+++ src/test/java/com/github/mangila/springbootrestfulservice/web/resource/v1/restassured/OrderResourceRestAssuredTest.java
@@ -1,7 +1,25 @@
 package com.github.mangila.springbootrestfulservice.web.resource.v1.restassured;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
 
 @Tag("restassured")
+@SpringBootTest
 public class OrderResourceRestAssuredTest {
+
+    @BeforeEach
+    public void beforeEach() {
+
+    }
+
+
+
 }
