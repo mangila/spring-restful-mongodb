dcdf65e066e4e6b51df0c52c0beb9e0e2aa9915e
Mangila <olsson.erik1993@gmail.com>
2021-09-02 12:15:15 +0200

    added RestAssured IT for CustomerResource

--- src/test/java/com/github/mangila/springbootrestfulservice/web/resource/v1/restassured/CustomerResourceRestAssuredTest.java
+++ src/test/java/com/github/mangila/springbootrestfulservice/web/resource/v1/restassured/CustomerResourceRestAssuredTest.java
@@ -1,19 +1,27 @@
 package com.github.mangila.springbootrestfulservice.web.resource.v1.restassured;
 
+import com.github.mangila.springbootrestfulservice.domain.CustomerDocument;
 import com.github.mangila.springbootrestfulservice.web.dto.v1.CustomerDto;
+import com.github.mangila.springbootrestfulservice.web.repository.v1.CustomerRepository;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 
-import java.util.Arrays;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @Tag("restassured")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -22,11 +30,27 @@ public class CustomerResourceRestAssuredTest {
     @LocalServerPort
     private int port;
 
+    @Autowired
+    private CustomerRepository repository;
+
+    private String testId;
+
     @BeforeEach
-    void afterEach() {
+    void beforeEach() {
         RestAssured.baseURI = "http://localhost";
         RestAssured.port = this.port;
         RestAssured.basePath = "/api/v1";
+
+        CustomerDocument c = new CustomerDocument();
+        c.setName("Heimdal");
+        c.setRegistration(LocalDate.now());
+        c.setOrderHistory(new ArrayList<>());
+        this.testId = repository.insert(c).getId();
+    }
+
+    @AfterEach
+    void afterEach() {
+        this.repository.deleteAll();
     }
 
     @Test
@@ -40,20 +64,61 @@ public class CustomerResourceRestAssuredTest {
                 .statusCode(HttpStatus.OK.value())
                 .extract().response();
         CustomerDto[] body = response.getBody().as(CustomerDto[].class);
+        assertEquals(body.length, 1);
     }
 
     @Test
     void findById() {
-
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .get("customer/" + this.testId)
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .extract().response();
+        CustomerDto body = response.getBody().as(CustomerDto.class);
+        assertEquals(body.getName(), "Heimdal");
     }
 
     @Test
-    void update() {
+    void insert() {
+        CustomerDto c = new CustomerDto();
+        c.setName("Thor");
 
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .body(c)
+                .post("customer")
+                .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .extract().response();
+        String location = response.getHeaders()
+                .getValue(HttpHeaders.LOCATION);
+        assertNotNull(location);
     }
 
     @Test
     void deleteById() {
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .delete("customer/" + this.testId)
+                .then()
+                .statusCode(HttpStatus.NO_CONTENT.value())
+                .extract().response();
+
+        response = given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .when()
+                .get("customer/" + this.testId)
+                .then()
+                .statusCode(HttpStatus.NOT_FOUND.value())
+                .extract().response();
 
     }
 
