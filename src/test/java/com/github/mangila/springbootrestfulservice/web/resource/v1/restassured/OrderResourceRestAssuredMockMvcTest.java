7a29ee5c9f937cba025728cd6d1048392b59289b
Mangila <olsson.erik1993@gmail.com>
2021-09-08 20:56:38 +0200

    added crud methods for RestAssuredMockMVCTest for Order domain

--- src/test/java/com/github/mangila/springbootrestfulservice/web/resource/v1/restassured/OrderResourceRestAssuredMockMvcTest.java
+++ src/test/java/com/github/mangila/springbootrestfulservice/web/resource/v1/restassured/OrderResourceRestAssuredMockMvcTest.java
@@ -1,6 +1,16 @@
 package com.github.mangila.springbootrestfulservice.web.resource.v1.restassured;
 
+import com.github.mangila.springbootrestfulservice.web.resource.v1.CustomerResource;
+import com.github.mangila.springbootrestfulservice.web.resource.v1.OrderResource;
+import com.github.mangila.springbootrestfulservice.web.service.v1.OrderService;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 /**
  * given() — specifies the HTTP request details
@@ -8,5 +18,38 @@ import org.junit.jupiter.api.Tag;
  * then() — validates the HTTP response
  */
 @Tag("restassured")
+@WebMvcTest(OrderResource.class)
 public class OrderResourceRestAssuredMockMvcTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private OrderService service;
+
+    @BeforeEach
+    void beforeEach() {
+        RestAssuredMockMvc.mockMvc(this.mockMvc);
+    }
+
+    @Test
+    void findAll() {
+
+    }
+
+    @Test
+    void findById() {
+
+    }
+
+    @Test
+    void insert() {
+
+    }
+
+    @Test
+    void deleteById() {
+
+    }
+
 }
