9d720f87d6db6e2a6fad5773333ee293b844dbd7
Mangila <olsson.erik1993@gmail.com>
2021-09-10 16:11:05 +0200

    added jackson JavaTimeModule dependency. And changed to read from json file for database seeding.

--- src/main/java/com/github/mangila/springbootrestfulservice/DatabaseSeeder.java
+++ src/main/java/com/github/mangila/springbootrestfulservice/DatabaseSeeder.java
@@ -1,6 +1,8 @@
 package com.github.mangila.springbootrestfulservice;
 
-import com.github.mangila.springbootrestfulservice.domain.Address;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.github.mangila.springbootrestfulservice.domain.CustomerDocument;
 import com.github.mangila.springbootrestfulservice.domain.OrderDocument;
 import com.github.mangila.springbootrestfulservice.web.repository.v1.CustomerRepository;
@@ -11,11 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
+import java.io.IOException;
+import java.net.URL;
 import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.stream.Stream;
 
 @Component
 @Profile("dev")
@@ -23,6 +23,7 @@ import java.util.stream.Stream;
 public class DatabaseSeeder implements InitializingBean {
 
     private final CustomerRepository customerRepository;
+
     private final OrderRepository orderRepository;
 
     @Autowired
@@ -32,42 +33,18 @@ public class DatabaseSeeder implements InitializingBean {
     }
 
     @Override
-    public void afterPropertiesSet() {
-        Stream.of("Tor", "Freja", "Loke", "Oden")
-                .forEach(name -> {
-                    var c = new CustomerDocument();
-                    c.setName(name);
-                    c.setRegistration(LocalDate.of(
-                            ThreadLocalRandom.current().nextInt(2010, 2020),
-                            ThreadLocalRandom.current().nextInt(1, 12),
-                            ThreadLocalRandom.current().nextInt(1, 28)
-                    ));
-                    c.setOrderHistory(new ArrayList<>());
-                    customerRepository.insert(c);
-                    for (int i = 0; i < ThreadLocalRandom.current().nextInt(5, 10); i++) {
-                        var o = new OrderDocument();
-                        o.setAmount(ThreadLocalRandom.current().nextInt(200, 800));
-                        o.setProducts(this.getRandomProducts());
-                        o.setAddress(new Address(
-                                "Asgard road " + ThreadLocalRandom.current().nextInt(1, 100),
-                                "Asgard"));
-                        var orderId = orderRepository.insert(o).getId();
-                        c = customerRepository.findByName(name);
-                        c.getOrderHistory().add(orderId);
-                        customerRepository.save(c);
-                    }
+    public void afterPropertiesSet() throws IOException {
+        ObjectMapper mapper = new ObjectMapper()
+                .registerModule(new JavaTimeModule());
+        URL url = DatabaseSeeder.class.getResource("/customer-dev-schema.json");
+        var customers = mapper
+                .readValue(url, new TypeReference<List<CustomerDocument>>() {
                 });
+        customerRepository.insert(customers);
+        url = DatabaseSeeder.class.getResource("/order-dev-schema.json");
+        var orders = mapper
+                .readValue(url, new TypeReference<List<OrderDocument>>() {});
+        orderRepository.insert(orders);
         log.info("Database seeded with dev data.");
-    }
-
-    public List<String> getRandomProducts() {
-        var products = List.of("T-Shirt", "Jeans", "Sweatpants",
-                "Trousers", "Strawberries", "Meatballs",
-                "Milk", "Watermelon", "Salomon", "Red Meat");
-        var l = new ArrayList<String>();
-        for (int i = 0; i <= 5; i++) {
-            l.add(products.get(ThreadLocalRandom.current().nextInt(0, 9)));
-        }
-        return l;
     }
 }
