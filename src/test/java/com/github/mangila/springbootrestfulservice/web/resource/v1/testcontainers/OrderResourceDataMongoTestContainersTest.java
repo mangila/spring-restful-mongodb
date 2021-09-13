0692846308013995e16569cc8e91009f134abbe8
Mangila <olsson.erik1993@gmail.com>
2021-09-13 12:50:05 +0200

    added OrderResource datamongo testcontainer test

--- src/test/java/com/github/mangila/springbootrestfulservice/web/resource/v1/testcontainers/OrderResourceDataMongoTestContainersTest.java
+++ src/test/java/com/github/mangila/springbootrestfulservice/web/resource/v1/testcontainers/OrderResourceDataMongoTestContainersTest.java
@@ -1,13 +1,16 @@
 package com.github.mangila.springbootrestfulservice.web.resource.v1.testcontainers;
 
+import com.github.mangila.springbootrestfulservice.domain.Address;
+import com.github.mangila.springbootrestfulservice.domain.OrderDocument;
 import com.github.mangila.springbootrestfulservice.web.repository.v1.OrderRepository;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 
 @Tag("testcontainers")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DataMongoTest(excludeAutoConfiguration = EmbeddedMongoAutoConfiguration.class)
 public class OrderResourceDataMongoTestContainersTest extends SeededMongoDBTestContainer {
 
@@ -17,9 +20,45 @@ public class OrderResourceDataMongoTestContainersTest extends SeededMongoDBTestC
     @Autowired
     private OrderRepository repository;
 
+    @Order(100)
     @Test
     void findAll() {
+        Assertions.assertEquals(11, this.repository.findAll().size());
+        Assertions.assertEquals(11, this.repository.count());
+    }
 
+    @Order(200)
+    @Test
+    void findById() {
+        OrderDocument o = this.repository.findById(this.orderId).get();
+        Assertions.assertNotNull(o);
+        Assertions.assertEquals("Minas Tirith road 101", o.getAddress().getStreet());
+        Assertions.assertEquals("Gondor", o.getAddress().getCity());
+        Assertions.assertEquals(750, o.getAmount());
+    }
+
+    @Order(300)
+    @Test
+    void insert() {
+        OrderDocument o = new OrderDocument();
+        o.setProducts(Lists.newArrayList("Sword", "Shield"));
+        Address a = new Address();
+        a.setStreet("Minas Tirith road 77");
+        a.setCity("Gondor");
+        o.setAddress(a);
+        o.setAmount(2000);
+        this.repository.insert(o);
+
+        Assertions.assertEquals(12, this.repository.findAll().size());
+        Assertions.assertEquals(12, this.repository.count());
+    }
+
+    @Order(400)
+    @Test
+    void deleteById() {
+        this.repository.deleteById(this.orderId);
+        Assertions.assertEquals(11, this.repository.findAll().size());
+        Assertions.assertEquals(11, this.repository.count());
     }
 
 }
