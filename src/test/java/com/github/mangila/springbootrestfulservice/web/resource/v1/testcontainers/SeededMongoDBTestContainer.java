3d5d82a1582ed00fb555e081180bd38ae5725cf7
Mangila <olsson.erik1993@gmail.com>
2021-10-08 12:25:42 +0200

    added logging for MongoDB Testcontainer

--- src/test/java/com/github/mangila/springbootrestfulservice/web/resource/v1/testcontainers/SeededMongoDBTestContainer.java
+++ src/test/java/com/github/mangila/springbootrestfulservice/web/resource/v1/testcontainers/SeededMongoDBTestContainer.java
@@ -1,17 +1,20 @@
 package com.github.mangila.springbootrestfulservice.web.resource.v1.testcontainers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 import java.io.IOException;
 
 @Testcontainers
+@Slf4j
 public abstract class SeededMongoDBTestContainer {
 
     private static final MongoDBContainer container =
@@ -25,6 +28,7 @@ public abstract class SeededMongoDBTestContainer {
                             "/tmp/order-test-mongoimport-schema.json",
                             BindMode.READ_ONLY
                     )
+                    .withLogConsumer(new Slf4jLogConsumer(log))
                     // 'testcontainers.reuse.enable=true' flag must be set in testcontainers.properties
                     .withReuse(true);
 
